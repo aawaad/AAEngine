05b32c61233344c515766e4fcc1d92eef33b6c42
aawaad <ayman.awaad@students.plymouth.ac.uk>
2016-03-02 11:31:59 +0000

    Added TODO list to game.h

--- src/game.h
+++ src/game.h
@@ -4,6 +4,107 @@
 #include "platform.h"
 #include "asset.h"
 
+/*
+    TODO:
+
+    Maths library (largely finished as another project)
+      - Still needs capsules and their intersections
+      - + OBBs
+      - various other collision methods
+      - curves (bezier etc)
+
+    Memory Allocator
+      x Basic stack allocation
+      - Temporary memory blocks
+      - Dynamic allocation (free, check for free space, merge adjacent free space)
+      - Contiguous block allocation?
+
+    Renderer
+      x Take input/commands from a buffer sent from the game layer
+      - Light management
+      - Camera management? (or should this be purely in the game layer which just sends a view matrix?)
+      - Frustrum culling (needs bounding spheres/scene hierarchy)
+      - Material / shader switching support
+      - Debug drawing
+      - Sorting
+        - Transparency?
+
+    Assets
+      x Basic art loading (meshes, textures)
+        - Better OBJ loader?
+        - More image formats? (unnecessary with pack file)
+        - Fonts/text
+      - Load from pack file
+        - Custom file format
+      - Streaming (dynamic allocation)
+      - Resource management (i.e. don't load the same file multiple times (fixed with pack file?))
+
+    Entity System
+      - Tracking (table, IDs etc)
+      - Updating
+      - Collision
+        - Collision flags
+        - General utility (get relative/canonical position, get ground point, etc)
+      - Picking
+      - Misc. game features?
+
+    HID System (Input)
+      x Basic input
+      x Buttons statically assigned to actions
+      - Proper input management and key reassignment (e.g. ability to reassign keys to whatever action)
+
+    Audio
+      - Basic audio loading and playback
+      - Sound effect triggers
+      - Music
+      - Basic mixer
+        - Volume
+        - Pitch shifting
+
+    Debugging
+      - Logging
+      - Graphs
+      - In-game console?
+      - Misc. drawing
+
+    Scene Management
+      - Scene graph
+      - Bounding spheres
+      - Portals?
+      - World chunks?
+      - High/low entity update frequency?
+
+    Physics
+      - Simulation
+        - Collision
+        - Force
+        - Friction
+        - Torque
+
+    Multithreading
+      - Work queue?
+      - Asset streaming?
+      - Audio streaming?
+
+    Animation
+      - Skeletal animation?
+        - Inverse kinematics?
+      - Particle systems?
+
+    Networking?
+
+    AI?
+      - Basic A* path finding
+      - Navigation meshes?
+        - Generation
+
+    Misc.
+      - Random number generation
+      - SIMD optimisation
+      - Move away from CRT?
+
+*/
+
 struct player
 {
     vec3 position;
